8176ae9eaf0b57dab4b0d125e285f3e76045506e
Tajima Junpei <p.baleine@gmail.com>
2013-01-08 20:36:58 +0900

    submoduleを更新、helmのyasnppetのインタフェースを更新

--- setup-helm.el
+++ setup-helm.el
@@ -5,8 +5,8 @@
 (setq helm-c-yas-space-match-any-greedy t)
 
 (require 'helm-c-moccur)
-(setq
- helm-c-moccur-enable-initial-pattern t)
+(setq helm-c-moccur-enable-initial-pattern t)
+(setq helm-c-yas-display-key-on-candidate t)
 (add-hook 'dired-mode-hook
 	  '(lambda ()
 	     (local-set-key (kbd "O") 'helm-c-moccur-dired-do-moccur-by-moccur)))
--- setup-js2-mode.el
+++ setup-js2-mode.el
@@ -1,9 +1,7 @@
-(setq-default js2-global-externs '("module" "require" "jQuery" "$" "_" "buster" "sinon" "assert" "refute" "setTimeout" "clearTimeout" "setInterval" "clearInterval" "location" "__dirname" "console" "JSON" "describe" "it" "beforeEach" "afterEach"))
+(setq-default js2-global-externs '("module" "require" "jQuery" "$" "_" "buster" "sinon" "assert" "refute" "setTimeout" "clearTimeout" "setInterval" "clearInterval" "location" "__dirname" "console" "JSON" "describe" "it" "before" "after" "beforeEach" "afterEach" "expect"))
 
 (require 'js2-mode)
 (require 'js2-refactor)
-
-(message "loading js2 setup")
 
 (require 'js2-imenu-extras)
 (js2-imenu-extras-setup)
