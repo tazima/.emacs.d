2e3ae5b48eeb671280e30ccb13e46fd0070a3176
Tajima Junpei <p.baleine@gmail.com>
2013-08-09 17:29:46 +0900

    update js2 setup

--- setup-js2-mode.el
+++ setup-js2-mode.el
@@ -9,4 +9,18 @@
 (setq js2-mode-hook
   '(lambda () (progn (set-variable 'indent-tabs-mode nil))))
 
+;; Use lambda for anonymous functions
+(font-lock-add-keywords
+ 'js2-mode `(("\\(function\\) *("
+              (0 (progn (compose-region (match-beginning 1)
+                                        (match-end 1) "\u0192")
+                        nil)))))
+
+;; Use right arrow for return in one-line functions
+(font-lock-add-keywords
+ 'js2-mode `(("function *([^)]*) *{ *\\(return\\) "
+              (0 (progn (compose-region (match-beginning 1)
+                                        (match-end 1) "\u2190")
+                        nil)))))
+
 (provide 'setup-js2-mode)
